95f2f19ea7fd66ab263ba9cc0ca03815e9d8f72d
Trystan Khang Nguyen <2161845@cegepmontpetit.ca>
2025-08-25 18:24:28 -0400

    feat : initialize CareerCompass API with EF Core, Identity, and CV management
    This commit includes :
    -Creation of new ASP.NET Core Web API project
    -Installation of EF Core, auth and API packages
    -Implementation of CV and User Models
    -Creation of CVsController
    -Creation of DbContext
    -Configuration of Program.cs

---
+++ WebApplication1/Controllers/CVsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CareerCompass.Data;
+using CareerCompass.Models;
+
+namespace CareerCompass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CVsController : ControllerBase
+    {
+        private readonly CareerCompassContext _context;
+
+        public CVsController(CareerCompassContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CVs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CV>>> GetCV()
+        {
+            return await _context.CV.ToListAsync();
+        }
+
+        // GET: api/CVs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CV>> GetCV(int id)
+        {
+            var cV = await _context.CV.FindAsync(id);
+
+            if (cV == null)
+            {
+                return NotFound();
+            }
+
+            return cV;
+        }
+
+        // PUT: api/CVs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCV(int id, CV cV)
+        {
+            if (id != cV.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cV).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CVExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CVs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CV>> PostCV(CV cV)
+        {
+            _context.CV.Add(cV);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCV", new { id = cV.Id }, cV);
+        }
+
+        // DELETE: api/CVs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCV(int id)
+        {
+            var cV = await _context.CV.FindAsync(id);
+            if (cV == null)
+            {
+                return NotFound();
+            }
+
+            _context.CV.Remove(cV);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CVExists(int id)
+        {
+            return _context.CV.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApplication1/Data/CareerCompassContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CareerCompass.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CareerCompass.Data
+{
+    public class CareerCompassContext : IdentityDbContext<User>
+    {
+        public CareerCompassContext (DbContextOptions<CareerCompassContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CareerCompass.Models.CV> CV { get; set; } = default!;
+    }
+}
